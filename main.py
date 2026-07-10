1bb67ec145254c8be864f7b2f9196be6192c3bf3
atasamiloglu <samilogluatacelik005@gmail.com>
2026-07-10 18:29:43 +0300

    Add summary statistics analysis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.data_loader import load_data
 from src.data_cleaning import inspect_data, clean_data
+from src.analysis import summary_statistics
 
 
 def main():
@@ -7,6 +8,8 @@ def main():
     inspect_data(df)
     df = clean_data(df)
     inspect_data(df)
+    
+    print(summary_statistics(df))
 
 
 if __name__ == "__main__":
